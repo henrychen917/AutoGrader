3b4b53cefdc4762be0b7af3ad8e8c77e10d2031a
henrychen917 <henrychen917@gmail.com>
2023-09-25 12:15:44 +0900

    test cases

---
+++ test/std1/std1_test.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	char str[100];
+	scanf("%s", str);
+	printf("%s\n", str);
+	scanf("%s", str);
+	printf("%s\n", str);
+	scanf("%s", str);
+	printf("%s\n", str);
+	return 0;
+}
